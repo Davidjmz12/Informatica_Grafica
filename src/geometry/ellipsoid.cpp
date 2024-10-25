6d4e543237170bc07e854bd03daf4a1b38068739
Davidjmz12 <davidjmzom1@gmail.com>
2024-10-25 11:34:35 +0200

    Working well ?

--- src/geometry/ellipsoid.cpp
+++ src/geometry/ellipsoid.cpp
@@ -1,13 +1,23 @@
 #include "geometry/ellipsoid.hpp"
 
-std::tuple<bool,double> solve_equation_second_degree(double a, double b, double c)
+std::vector<double> solve_equation_second_degree(double a, double b, double c)
 {
+    std::vector<double> solutions;
     // Case a=0: Equation of one degree.
     if (eqD(a,0))
+    {
         if (eqD(b,0))
-            throw std::invalid_argument("Parameters a and b cannot be 0 at the same time");
+        {
+            return solutions;
+        }
         else
-            return std::make_tuple(true, -c/b);
+        {
+            solutions.push_back(-c/b);
+            return solutions;
+        }
+    }
+
+
 
     // Make a > 0 
     if (a < 0)
@@ -20,11 +30,19 @@ std::tuple<bool,double> solve_equation_second_degree(double a, double b, double 
     // Compute the solution
     double disc = sqrt(pow(b,2) - 4*a*c);
     if (eqD(disc,0))
-        return std::make_tuple(true,-b/(2*a));
-    else if (disc > 0)
-        return std::make_tuple(true,(-b - disc)/(2*a));
-    else
-        return std::make_tuple(false,0.0);
+    {
+        solutions.push_back(-b/(2*a));
+        return solutions;
+    } else if (disc > 0)
+    {
+        solutions.push_back((-b + disc)/(2*a));
+        solutions.push_back((-b - disc)/(2*a));
+        return solutions;
+    } else 
+    {
+        return solutions;
+    }
+
 }
 
 Ellipsoid::Ellipsoid(double a, double b, double c, Point center, Property properties)
@@ -36,61 +54,52 @@ Ellipsoid::Ellipsoid(double a, double b, double c, Point center, Property proper
 
 bool Ellipsoid::intersect_with_ray(const Ray& r, Intersection& intersection) const
 {
-    //Traslation to the canonical center of the ellipsoid
-    Ray ray = Ray(r.get_point()-(this->_center-Point()),r.get_direction());
+    Vector dir_ray = r.get_direction();
+    Point point_centered = Point(r.get_point()-this->_center);
 
-    //Compute the necessary data
-    Vector direction = ray.get_direction();
-    double dx = direction[0], dy = direction[1], dz = direction[2];
-    double A = 1/(pow(this->_a,2)), B = 1/(pow(this->_b,2)), C = 1/(pow(this->_c,2));
-    Point point = ray.get_point();
-    double ox = point[0], oy = point[1], oz = point[2];
+    double c = pow(point_centered[0]/this->_a,2) + 
+               pow(point_centered[1]/this->_b,2) + 
+               pow(point_centered[2]/this->_c,2) - 1;
 
-    //Compute the coefficients
-    double a = A*pow(dx,2) + B*pow(dy,2) + C*pow(dz,2);
-    double b = 2*(A*dx*ox + B*dy*oy + C*dz*oz);
-    double c = A*pow(ox,2) + B*pow(oy,2) + C*pow(oz,2) - 1;
+    double b =  2*dir_ray[0]*point_centered[0]/pow(this->_a,2) +
+                2*dir_ray[1]*point_centered[1]/pow(this->_b,2) +
+                2*dir_ray[2]*point_centered[2]/pow(this->_c,2); 
 
-    // Check if exists solution
-    if (eqD(a,0) && eqD(b,0))
+    double a =  pow(dir_ray[0]/this->_a,2) +
+                pow(dir_ray[1]/this->_b,2) +
+                pow(dir_ray[2]/this->_c,2);
+
+
+    //Calculate the solution
+    std::vector<double> solution = solve_equation_second_degree(a,b,c);
+    bool existSolution = solution.size() > 0;
+
+    // If there is no solution, return false
+    if(!existSolution)
         return false;
-    else
-    {
-        //Calculate the solution
-        std::tuple solution = solve_equation_second_degree(a,b,c);
-        bool existSolution;
-        double numericSolution;
-        std::tie(existSolution,numericSolution) = solution;
 
-        //Construct intersection
-        if (existSolution)
-        {
-            Point point = ray.evaluate(numericSolution);
-            point = point + (this->_center - Point());
-            intersection = Intersection(numericSolution, this->normal(point), point, this->_properties, ray.get_direction());
-        }
+    double distance = 0;
+    for(double d : solution)
+        if (gtD(d,0))
+            distance = d;
 
-        return existSolution;
-    }
+    // If the ray is in the ellipsoid or behind it, return false
+    if (distance == 0)
+        return false;
+
+
+    Point point_int = r.evaluate(distance);
+    intersection = Intersection(distance, this->normal(point_int), point_int, this->_properties, r.get_direction());
+
+    return existSolution;    
 }
 
 Vector Ellipsoid::normal(Point p) const
 {
-    //Check if the point belongs to the ellipsoid
-    if (!this->is_in_ellipsoid(p))
-        throw std::runtime_error("The given point is not in the ellipsoid.");
-
     //Compute the normal vector
     return Vector(  (p[0]-this->_center[0])/pow(this->_a,2),
                     (p[1]-this->_center[1])/pow(this->_b,2),
                     (p[2]-this->_center[2])/pow(this->_c,2)).normalize();
-}
-
-bool Ellipsoid::is_in_ellipsoid(Point p) const
-{
-    return  eqD(pow(p[0]-this->_center[0],2)/pow(this->_a,2) +
-                    pow(p[1]-this->_center[1],2)/pow(this->_b,2) + 
-                    pow(p[2]-this->_center[2],2)/pow(this->_c,2),1);
 }
 
 std::ostream& operator<<(std::ostream& os, const Ellipsoid& e)
--- tests/geometry_tests/test_ellipsoid.cpp
+++ tests/geometry_tests/test_ellipsoid.cpp
@@ -19,20 +19,20 @@ int main()
     Intersection res = Intersection();
 
     t.addTest("1", Test::EXPECT_EQ(e.intersect_with_ray(r,res),true));
-    t.addTest("2", Test::EXPECT_EQ(res,Intersection(1,Vector(-1,0,0),Point(-1,0,0),property)));
+    t.addTest("2", Test::EXPECT_EQ(res,Intersection(1,Vector(-1,0,0),Point(-1,0,0), property, r.get_direction())));
 
     Ellipsoid e2 = Ellipsoid(3,2,1,Point(2,3,5),property);
     Ray r2 = Ray(Point(6,0,0),Vector(-4,3,4));
 
     t.addTest("3", Test::EXPECT_EQ(e2.intersect_with_ray(r2,res),true));
-    t.addTest("4", Test::EXPECT_EQ(res,Intersection(sqrt(41),Vector(0,0,-1),Point(2,3,4),property)));
+    t.addTest("4", Test::EXPECT_EQ(res,Intersection(sqrt(41),Vector(0,0,-1),Point(2,3,4),property, r.get_direction())));
 
     Ray r3 = Ray(Point(6,0,0),Vector(0,0,1));
     t.addTest("5", Test::EXPECT_EQ(e2.intersect_with_ray(r3,res),false));
 
     Ray r4 = Ray(Point(6,0,0),Vector(-1,3,5));
     t.addTest("6", Test::EXPECT_EQ(e2.intersect_with_ray(r4,res),true));
-    t.addTest("7", Test::EXPECT_EQ(res,Intersection(sqrt(35),Vector(1,0,0),Point(5,3,5),property)));
+    t.addTest("7", Test::EXPECT_EQ(res,Intersection(sqrt(35),Vector(1,0,0),Point(5,3,5), property, r.get_direction())));
 
 
 
