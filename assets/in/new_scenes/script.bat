43f9644ce83e99b3aa14d8b35edb8ce8ddc3d81b
Davidjm123 <davidjmzom1@gmail.com>
2025-01-05 21:34:20 +0100

    a

---
+++ assets/in/new_scenes/script.bat
@@ -0,0 +1,6 @@
+.\build\src\main.exe --threads 12 --rays 8  --task-size 32 --bounces 5 --max-depth 1 --file-in in/new_scenes/scene_1.xml --file-out out/plight8.ppm
+.\build\src\main.exe --threads 12 --rays 128  --task-size 32 --bounces 5 --max-depth 1 --file-in in/new_scenes/scene_1.xml --file-out out/plight128.ppm
+.\build\src\main.exe --threads 12 --rays 512  --task-size 32 --bounces 5 --max-depth 1 --file-in in/new_scenes/scene_1.xml --file-out out/plight512.ppm
+.\build\src\main.exe --threads 12 --rays 8  --task-size 32 --bounces 5 --max-depth 1 --file-in in/new_scenes/scene_2.xml --file-out out/alight8.ppm
+.\build\src\main.exe --threads 12 --rays 128  --task-size 32 --bounces 5 --max-depth 1 --file-in in/new_scenes/scene_2.xml --file-out out/alight128.ppm
+.\build\src\main.exe --threads 12 --rays 512  --task-size 32 --bounces 5 --max-depth 1 --file-in in/new_scenes/scene_2.xml --file-out out/alight512.ppm
